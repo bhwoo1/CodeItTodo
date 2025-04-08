057f2119c2bdc4aa7769fcbe8ad603fdc13a5c3c
Byeonghyeon <problemp123@gmail.com>
2025-04-08 14:27:51 +0900

    [fix] build시 오류 수정

--- src/app/items/[itemId]/page.tsx
+++ src/app/items/[itemId]/page.tsx
@@ -1,11 +1,11 @@
 import ItemPage from '@/app/components/items/ItemPage';
 import React from 'react'
 
-export default async function page({params}: {params: {itemId: number}}) {
+export default async function page({params}: {params: {itemId: string}}) {
     const {itemId} = await params;
   return (
     <div className='lg:mx-96 my-12'>
-        <ItemPage itemId={itemId} />
+        <ItemPage itemId={Number(itemId)} />
     </div>
   )
 }
