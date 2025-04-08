e722f5a4e48cb187ea0c533d0f25968ec4bfd3a1
Byeonghyeon <problemp123@gmail.com>
2025-04-08 14:32:57 +0900

    [fix] build 오류 수정

--- src/app/items/[itemId]/page.tsx
+++ src/app/items/[itemId]/page.tsx
@@ -1,8 +1,10 @@
+"use client"
+
 import ItemPage from '@/app/components/items/ItemPage';
 import React from 'react'
 
 
-export default async function Page({ params }: { params: { itemId: string } })  {
+export default function Page({ params }: { params: { itemId: string } })  {
   return (
     <div className='lg:mx-96 my-12'>
         <ItemPage itemId={Number(params.itemId)} />
