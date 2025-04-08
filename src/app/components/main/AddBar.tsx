da6dfc21519327909f1a7f58306ac469c09de458
Byeonghyeon <problemp123@gmail.com>
2025-04-08 09:34:21 +0900

    [feat] Todo AddBar

---
+++ src/app/components/main/AddBar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { FormEvent } from "react";
+
+function AddBar() {
+  const handleTodoAdd = (e: FormEvent) => {
+    e.preventDefault();
+  };
+  return (
+    <form className="flex flex-row gap-12 my-8 mr-0">
+      <input
+        type="text"
+        placeholder="할 일을 입력해주세요"
+        className="w-2/3 lg:w-1/2 h-12 px-4 ml-12 lg:ml-96 bg-slate-300 border-2 border-slate-800 rounded-full outline-none shadow-[4px_4px_#0F172A]"
+      />
+      <button className="h-12 cursor-pointer" onClick={handleTodoAdd}>
+        <div className="shadow-[4px_4px_#0F172A] rounded-full border-2 w-[50px] mr-12 lg:mr-96 md:w-[200px] bg-slate-200 h-12 flex text-center items-center justify-center">
+          <span className="block sm:hidden text-2xl font-bold">+</span>
+          <span className="hidden sm:block">+ 추가하기</span>
+        </div>
+      </button>
+    </form>
+  );
+}
+
+export default AddBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import AddBar from "./components/main/AddBar";
+
 export default function Home() {
   return (
     <div>
-
+      <AddBar />
     </div>
   );
 }
