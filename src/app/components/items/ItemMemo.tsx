a32bc194b5e758b4256d0c8e42e3b7d026815133
Byeonghyeon <problemp123@gmail.com>
2025-04-09 09:11:28 +0900

    [fix] memo null 에러 수정

--- src/app/components/items/ItemMemo.tsx
+++ src/app/components/items/ItemMemo.tsx
@@ -6,7 +6,7 @@ function ItemMemo({item, newMemo, onMemoUpdate}: {item: Item, newMemo: string|nu
     <div className="w-full h-[300px] mx-4 lg:mx-0 lg:my-4 lg:w-2/3 lg:h-[400px] bg-[url('/img/memo.png')] bg-cover bg-center rounded-4xl text-center flex flex-col items-center">
         <p className='my-6 text-amber-800'>Memo</p>
         <textarea 
-            value={newMemo || item?.memo}
+            value={(newMemo !== null ? newMemo : item?.memo) ?? ""}
             className="w-[300px] h-[200px] md:w-[600px] lg:w-[580px] lg:h-[300px]  resize-none rounded-lg text-center flex items-center justify-center p-[80px] lg:p-[130px]"
             onChange={(e) => onMemoUpdate(e.target.value)}
         />
